634ba6a5a6ee68334808320f5520e529f893853e
Quang823 <quangdmse173565@fpt.edu.vn>
2024-08-17 21:46:09 +0700

    Order,OrderDetail

---
+++ GarmentFactoryAPI/Models/OrderDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace GarmentFactoryAPI.DTOs
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double TotalPrice { get; set; }
+        public int UserId { get; set; }
+        public List<OrderDetailDTO> OrderDetails { get; set; }
+    }
+
+    public class OrderDetailDTO
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int ProductId { get; set; }
+    }
+}
