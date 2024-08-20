041816a5ef705bd587258e6bbcf3021ca62f8123
Truong Nguyen Ngoc Dat <dattnnse173524@fpt.edu.vn>
2024-08-20 16:13:02 +0700

    fix

--- GarmentFactoryAPI/Program.cs
+++ GarmentFactoryAPI/Program.cs
@@ -17,8 +17,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
-
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -92,6 +90,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 
 // Configure scoped services
 builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<RoleService>();
 
 var app = builder.Build();
 
---
+++ GarmentFactoryAPI/Services/RoleService.cs
@@ -0,0 +1,29 @@
+﻿using GarmentFactoryAPI.Data;
+using GarmentFactoryAPI.Models;
+using GermentFactoryAPI.Services;
+
+namespace GarmentFactoryAPI.Services
+{
+    public interface IRoleService
+    {
+        Task<bool> RoleExists(int roleId);
+    }
+    public class RoleService : IRoleService
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public RoleService(DataContext context)
+        {
+            _unitOfWork = new UnitOfWork(context);
+        }
+
+        // Existing methods...
+
+        public async Task<bool> RoleExists(int roleId)
+        {
+            // Check if a role with the given roleId exists in the database
+            var role = await _unitOfWork.RoleRepository.GetByIdAsync(roleId);
+            return role != null;
+        }
+    }
+}
