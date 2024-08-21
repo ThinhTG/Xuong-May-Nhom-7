28bfb51e93400cc52a4e430497d7104de19599af
ThinhTG <thinhtgse173566@fpt.edu.vn>
2024-08-21 11:47:07 +0700

    up Category

--- GarmentFactoryAPI/Interfaces/ICategoryService.cs
+++ GarmentFactoryAPI/Interfaces/ICategoryService.cs
@@ -7,10 +7,8 @@ namespace GarmentFactoryAPI.Interfaces
     {
         ICollection<CategoryDTO> GetCategories();
         CategoryDTO GetCategoryById(int categoryId);
-        ICollection<CategoryDTO> GetCategoriesByName(string categoryName);
         bool CreateCategory(CategoryDTO categoryDto);
         bool UpdateCategory(CategoryDTO categoryDto);
         bool DeleteCategory(int categoryId);
-        ICollection<CategoryDTO> GetAllCategoriesFromData();
     }
 }
